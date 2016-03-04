45262a2bde0713f53438c889040f6c202d2d7764
24547744m <24547744m@github.com>
2016-03-04 19:26:54 +0100

    primer commit proyecto con librerias

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by damuser on 4/03/16.
+ */
+
+
+
+var app = angular.module("TweetsApp", []);
+var bootstrap = angular.module('BoostrapModule', ['ui.bootstrap']);
+
---
+++ assets/js/controller.js
@@ -0,0 +1,15 @@
+/**
+ * Created by damuser on 4/03/16.
+ */
+
+
+app.controller("TweetsController", function($scope){
+
+    $scope.res = "resultado: ";
+    $scope.nums = [1, 4, 7, 9, 14];
+
+    $scope.test = function(){
+        return "testttttt";
+    };
+
+});
