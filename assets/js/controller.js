7a8c437acca936083a463eb907da97ca0ce7b068
24547744m <24547744m@github.com>
2016-03-10 19:00:06 +0100

    interfaz grafica mejorada

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -8,6 +8,33 @@ app.controller("TweetsController", function($scope){
     $scope.res = "resultado: ";
     $scope.nums = [1, 4, 7, 9, 14];
 
+    $scope.tweets = [
+        {
+            user: 19,
+            tweet: 'The Book of Trees'
+        },
+        {
+            user: 8,
+            tweet: 'Program or be Programmed'
+        },
+        {
+            user: 12,
+            tweet: 'Program or be Programmed'
+        },
+        {
+            user: 20,
+            tweet: 'The Book of Trees'
+        },
+        {
+            user: 11,
+            tweet: 'The Book of Trees'
+        },
+        {
+            user: 8,
+            tweet: 'Program or be Programmed'
+        }
+    ]
+
     $scope.test = function(){
         return "testttttt";
     };
